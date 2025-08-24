9abd7f45ae89abf77e97622eaf933760996f21d4
venkatesh <lankevenkatesh314@gmail.com>
2025-08-24 15:43:04 +0530

    ADD DATA

--- Query_Practice.sql
+++ Query_Practice.sql
@@ -17,3 +17,12 @@ SELECT FIRST_NAME,LAST_NAME,salary  FROM EMP ORDER BY salary ASC;
 4. List the details of the emps in asc order of the salary and desc of Jobs?
 */
 SELECT * FROM emp ORDER BY salary, job_id DESC;
+
+/*
+5. Display all the unique job groups in the descending order?  
+*/
+
+SELECT DISTINCT JOB_ID AS JOB_GROUP FROM emp ORDER BY JOB_ID; 
+
+SELECT DISTINCT job_id AS job_group FROM emp ORDER BY job_id;
+
